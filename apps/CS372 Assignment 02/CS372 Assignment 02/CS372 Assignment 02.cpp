155fb7c6a39da1547609913f1d4808c50937cf1b
UnluckyPaladinTTRPG <unluckypally@gmail.com>
2025-06-11 10:29:57 -0500

    Final commit for Assignment 02

--- apps/CS372 Assignment 02/CS372 Assignment 02/CS372 Assignment 02.cpp
+++ apps/CS372 Assignment 02/CS372 Assignment 02/CS372 Assignment 02.cpp
@@ -4,10 +4,13 @@
 #include <iostream>
 #include "Vector.hpp"
 #include "PolyVector.hpp"
+#include "Array.hpp"
 #include <chrono>
+
 
 void vecTest(Vector<int>&);
 void addElements(Vector<int>&, int&);
+void ArrTest();
 
 int main()
 {
@@ -18,6 +21,7 @@ int main()
 
     std::cout << "Capacity Vector: " << newVector.capacity() << std::endl;
     std::cout << "Capacity PolyVector: " << pVector.capacity() << std::endl;
+    
 }
 
 
@@ -42,4 +46,21 @@ void vecTest(Vector<int>& vec)
     end = std::chrono::steady_clock::now();
     elapsed_seconds = end - start;
     std::cout << "Elapsed time: " << elapsed_seconds.count() << "ms\n";
+}
+
+void ArrTest()
+{
+    Array<int> myArray(5);
+    myArray[0] = 1;
+    myArray[1] = 2;
+    myArray[2] = 3;
+    myArray[3] = 4;
+    myArray[4] = 5;
+    myArray.traverse();
+    myArray.push_back(3);
+    myArray.traverse();
+    myArray.pop_back();
+    myArray.traverse();
+    myArray.pop_back();
+    myArray.traverse();
 }
